8cda3f2026cf9d1d14bc02f23c22731a43f12cfc
Ethan Sanders <ethanedu5@gmail.com>
2018-11-16 17:00:30 -0600

    Added main content container dashboard

--- src/Components/Dashboard/Dashboard.js
+++ src/Components/Dashboard/Dashboard.js
@@ -1,12 +1,15 @@
 import React, { Component } from 'react';
 import Navbar from '../Navbar/Navbar';
+import "./Dashboard.css"
 
 class Dashboard extends Component {
     render() {
         return (
-            <div>
+            <div className="logged_in_main_content">
                 <Navbar history={this.props.history}/>
-                Dashboard
+                <div className="dashboard_main_section">
+                
+                </div>
             </div>
         );
     }
--- src/Components/Form/Form.js
+++ src/Components/Form/Form.js
@@ -4,7 +4,7 @@ import Navbar from "./../Navbar/Navbar";
 class Form extends Component {
     render() {
         return (
-            <div>
+            <div className="logged_in_main_content">
                 <Navbar history={this.props.history}/>
                 Form
             </div>
