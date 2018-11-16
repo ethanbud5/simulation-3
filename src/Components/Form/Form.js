ac241107f320ac8f1c0b5ab9d38e64ad80c0f8a9
Ethan Sanders <ethanedu5@gmail.com>
2018-11-16 16:43:01 -0600

    have a working navbar

--- src/Components/Form/Form.js
+++ src/Components/Form/Form.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
+import Navbar from "./../Navbar/Navbar";
 
 class Form extends Component {
     render() {
         return (
             <div>
+                <Navbar history={this.props.history}/>
                 Form
             </div>
         );
--- src/Components/Post/Post.js
+++ src/Components/Post/Post.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
+import Navbar from "./../Navbar/Navbar";
 
 class Post extends Component {
     render() {
         return (
             <div>
+                <Navbar history={this.props.history}/>
                 Post
             </div>
         );
